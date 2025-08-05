2b6ed4fd24a608bb77ae51ebc23021e5c8f97fdf
ANUSHA PRASAD <ANUSHA PRASAD@LAPTOP-PKQFT7B4>
2025-08-05 13:47:57 +0530

    second commit

---
+++ src/com/abc/in/Test2.java
@@ -0,0 +1,24 @@
+package com.abc.in;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		
+		String s ="Dell";
+		char [] c =s.toLowerCase().toCharArray();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(char c1:c)
+		{
+			
+			if(sb.indexOf(String.valueOf(c1)) == -1)   
+			{
+				
+				sb.append(c1);
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
